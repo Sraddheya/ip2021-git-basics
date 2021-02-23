3bf07eb8b638a9515b222d8c6658f154c5b6611f
Sraddheya <ggsraddheya@gmail.com>
2021-02-23 13:01:40 +0000

    fixed stuff

--- Conatins.java
+++ Conatins.java
@@ -1,2 +1,19 @@
-public​​class​​Contains​ {​public​​static​​int ​main​(​String​[] ​args​) {
-​int​[] ​num​ = (​1​, ​2​, ​3​, ​4​, ​5​);​int​​toFind​ = ​3​;​boolean​​found​ = ​true​;​for​ (​n​​:​ num) {​if​ (n = toFind) {                found = ​true​;​break            }       }​if​(found)​print​(toFind ​" is found."​);​else​print​(toFind + ​" is not found."​);    }}
+public​​class​​Contains​ {
+    public​​ static ​​int ​main​(​String​[] ​args​) {
+
+        int​[] ​num​ = [​1​, ​2​, ​3​, ​4​, ​5​];​
+        int​​ toFind​ = ​3​;​
+        boolean​​ found​ = ​True​;​
+
+        for​ (​n​​:​ num) {​
+		if​ (n == toFind) {
+            		found = ​true​;​
+			break
+		}       
+	}​
+		if​(found){
+			​print​(toFind ​" is found."​);​
+		else​
+			print​(toFind + ​" is not found."​);    
+	}
+}
